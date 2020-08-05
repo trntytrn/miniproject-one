5ac02f9ebe057917e556f640918f4f78aac7036f
Trinity Tran <41582245+trntytrn@users.noreply.github.com>
2020-08-05 17:43:19 -0400

    added everything

--- miniproject-one/miniproject-one/ViewController.swift
+++ miniproject-one/miniproject-one/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var factOneLabel: UILabel!
+    @IBOutlet weak var factTwoLabel: UILabel!
+    @IBOutlet weak var factThreeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    var factOne = "I'm starting a sticker business with some Kode With Klossy friends"
+    var factTwo = "I coded a website with resources for the Black Lives Matter movement (notatrend.site)"
+    var factThree = "I've gotten stabbed in the stomach with a pencil in first grade"
+    @IBAction func aboutButtonPressed(_ sender: UIButton) {
+        factOneLabel.text = factOne
+        factTwoLabel.text = factTwo
+        factThreeLabel.text = factThree
+       }
+       
 
 }
 
+//I'm starting a sticker business with some Kode With Klossy friends
+
+//I coded a website with resources for the Black Lives Matter movement (notatrend.site)
+
+//I've gotten stabbed in the stomach with a pencil in first grade
